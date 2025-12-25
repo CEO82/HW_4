202cb3a6df2713a86626162ab1c113b3a865d58c
CEO82 <git-test@test.com>
2025-12-25 21:32:49 +0000

    GB_HW_4_2 - done, GB_HW_4_3 added

--- SandBox.py
+++ SandBox.py
@@ -1,8 +1,16 @@
 import random
 
-numbers = [random.randint(1, 1000) for _ in range(10)]
+def check_list():
+    init_list = [random.randint(1, 100) for _ in range(15)]
+    print(f'\nInitial list is: {init_list}')
 
-print(numbers)
-print(numbers[1:])
-count = 1
-numbers_2 = [if i > numbers[count - 1:] for i in numbers[1:]]
+    result_list = [
+        curr for prev, curr in zip(init_list, init_list[1:])
+        if curr > prev
+    ]
+
+    return result_list
+
+
+if __name__ == '__main__':
+    print(f'\nThe Result list is: {check_list()}')
