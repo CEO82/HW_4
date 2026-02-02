9e13d7cc29426f23a702ef1972fd31451f453935
CEO82 <git-test@test.com>
2026-02-02 20:53:29 +0000

    GB_HW_4_6_3 done

--- GB_HW_4_6_3.py
+++ GB_HW_4_6_3.py
@@ -17,8 +17,8 @@ def generator_for_count(start_n: Decimal, step: Decimal, finish_n: Decimal):
     return takewhile(lambda x: x <= finish_n if step > 0 else x >= finish_n, count(start_n, step))
 
 def generator_for_cycle(list_for_iteration: list, numbers_of_iterations: int):
-    mixed = zip(count(1), list_for_iteration)
-    return takewhile(lambda x: x[0] <= numbers_of_iterations, cycle(mixed))
+    mixed = zip(count(1), cycle(list_for_iteration))
+    return takewhile(lambda x: x[0] <= numbers_of_iterations, mixed)
 
 def print_for_generators(generator_obj):
     for p_o in generator_obj: # p_o printing object
@@ -26,30 +26,78 @@ def print_for_generators(generator_obj):
 
 def continue_or_not(function_name: str):
     user_wish = input(
-        f'If you want to start or continue function {function_name} input any symbol \nif you want to finish function input "q" or "Q"')
+        f'If you want to start or continue function {function_name} input any symbol \nif you want to finish function input "q" or "Q" -> ')
     if user_wish.lower() == 'q':
         return False
     else:
         return True
 
 def input_for_count():
+    while True:
+        try:
+            start_num = Decimal(input(f'Please enter a starting number: '))
+            step = Decimal(input(f'Please enter a number for generator step: '))
+            finish_num = Decimal(input(f'Please enter a last number: '))
+            if step == 0:
+                print(f'You are entered step = 0, this is forbidden, please repeat input')
+                continue
+            elif step < 0 and finish_num > start_num:
+                print(f'You are entered negative step and finish number greater than start number \nIt is prohibited, please repeat input')
+                continue
+            elif step > 0 and finish_num < start_num:
+                print(
+                    f'You are entered positive step and finish number smaller than start number \nIt is prohibited, please repeat input')
+                continue
+            else:
+                return start_num, step, finish_num
 
-    pass
+        except (ValueError, InvalidOperation):
+            print(f'\nYou are entered something wrong please repeat input ')
+            continue
+
+def input_for_cycle():
+    while True:
+        try:
+            cycle_reps = int(input(f'Please enter numbers of repeats for cycle ->  '))
+        except ValueError:
+            print(f'You are entered something incorrectly, please repeat input\n')
+            continue
+        if cycle_reps <= 0:
+            print(f'You are entered number of repeats less or equal to 0, this is forbidden, please repeat input\n')
+            continue
+        elif cycle_reps > 255:
+            print(f'Maximum numbers of repeats is 255, please enter number between 0 and 256\n')
+            continue
+        else:
+            return cycle_reps
+
+
 
 def count_funct():
-    function_name = 'count function'
+    function_name = '<<count function>>'
     while True:
         if continue_or_not(function_name) is False:
             print(f'Function {function_name} is finished')
+            break
         else:
+            start_num, step, finish_num = input_for_count()
+            print_for_generators(generator_for_count(start_num, step, finish_num))
+            continue
 
-
-    pass
 
 def cycle_funct():
+    function_name = '<<cycle function>>'
+    list_for_iteration = ['Green', 'Yellow', 'Red']
+    while True:
+        if continue_or_not(function_name) is False:
+            print(f'Function {function_name} is finished')
+            break
+        else:
+            numbers_of_iterations = input_for_cycle()
+            print_for_generators(generator_for_cycle(list_for_iteration, numbers_of_iterations))
 
-    pass
+
 
 if __name__ == '__main__':
-
-    print()
+    count_funct()
+    cycle_funct()
