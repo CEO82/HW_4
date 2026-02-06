ea6090ee9944d8cc73de65920d54b8273528b202
CEO82 <git-test@test.com>
2026-02-06 04:55:43 +0000

    GB_4_GPT_1 2

--- GB_4_GPT_1.py
+++ GB_4_GPT_1.py
@@ -5,3 +5,29 @@
 Использует yield (не список, не return списка)
 После достижения n генератор должен корректно завершиться'''
 
+
+def num_generator(last_numb: int):
+    for n in range(1,last_numb):
+        yield n
+
+def usr_input():
+    while True:
+        try:
+            last_numb = int(input(f'Enter last number: '))
+            if last_numb > 1:
+                return last_numb
+            else:
+                print(f'\nWrong enter, repeat\n')
+                continue
+        except ValueError:
+            print(f'\nWrong enter, repeat\n')
+            continue
+
+
+
+
+if __name__ == '__main__':
+    for i in num_generator(7):
+        print(i)
+
+
