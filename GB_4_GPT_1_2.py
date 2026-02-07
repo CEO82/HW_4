b3199885c3e05e29405653a03b7b274c6e54d6c6
CEO82 <git-test@test.com>
2026-02-07 17:43:29 +0000

    GB_4_GPT_1_2 done GB_4_GPT_2_1 started

--- GB_4_GPT_1_2.py
+++ GB_4_GPT_1_2.py
@@ -19,20 +19,62 @@
 
 '''
 
-def return_n(last_number: int):
+def return_numbers(last_number: int):
+    name_1 = 'return'
+    res_list = []
     for i in range(1, last_number + 1):
-        return i
+        res_list.append(i)
+
+    return res_list
 
 
 
-def generate_n(last_number: int):
+def yield_numbers(last_number: int):
+    name_2 = 'yield'
+    for m in range(1, last_number + 1):
+        yield  m
 
-    pass
+
+def user_input():
+    while True:
+        try:
+            last_numb = int(input(f'Enter last number: '))
+            if last_numb > 1:
+                return last_numb
+
+            else:
+                print(f'\nWrong enter, repeat\n')
+                continue
+        except ValueError:
+            print(f'\nWrong enter, repeat\n')
+            continue
+
+def print_result(data):
+    print(f'\n******')
+    print(type(data))
+    print(data)
+    print(f'******')
 
 
+def print_result_by_for_in(data):
+    print(f'\n******')
+    for k in data:
+        print(k)
+
+    print(f'******')
+
+
+def main_function():
+    u_input = user_input()
+    res_return = return_numbers(u_input)
+    res_yield = yield_numbers(u_input)
+    print_result(res_return)
+    print_result(res_yield)
+    print_result_by_for_in(res_return)
+    print_result_by_for_in(res_yield)
 
 
 
 if __name__ == '__main__':
-
+    main_function()
     pass
