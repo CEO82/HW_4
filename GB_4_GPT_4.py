7135aebcf9b5947d02ebf4a40714406c8137ffc2
CEO82 <git-test@test.com>
2026-03-01 20:26:08 +0000

    GB_4_GPT_4.py - done

--- GB_4_GPT_4.py
+++ GB_4_GPT_4.py
@@ -18,7 +18,45 @@ safe_accumulator()
 
 '''
 
+def resilient_accumulator():
+    accum = 0
+    print(f'\n*******************\nCurrent sum -> {accum}\n*******************')
+    while True:
+        try:
+            x = yield accum
+            if x is not None:
+                accum += x
+                print(f'*******************\nReceived     -> {x}')
+                print(f'Current sum -> {accum}\n*******************')
+        except ValueError:
+            print('**********************\nInvalid value received\n**********************')
+
+        except GeneratorExit:
+            print(f'*******************\nFinal sum -> {accum}\nGenerator is closing\n*******************')
+            return print('Generator is closed')
+
+def gen_control():
+    gen = resilient_accumulator()
+    next(gen)
+    print(f'Program The "Accumulator" is started!')
+
+    while True:
+        continue_or_not = input(f'If you want to continue program, please enter Y or y, \nif not enter any other symbol -> ')
+        if continue_or_not.lower() != 'y':
+            print(f'Program is going to stop')
+            gen.close()
+            break
+
+        try:
+            num_input = float(input(f'please enter any number -> '))
+
+        except ValueError:
+            #print('You entered something wrong, please repeat')
+            gen.throw(ValueError)
+            continue
+        print(num_input)
+        gen.send(num_input)
 
 if __name__ == '__main__':
+    gen_control()
 
-    pass
