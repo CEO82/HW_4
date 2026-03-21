1a17d5a2ce334d68bf1bbc6a1bcd11e2b0985ad0
CEO82 <git-test@test.com>
2026-03-21 17:30:07 +0000

    GB_4_GPT_7 - done

--- GB_4_GPT_7.py
+++ GB_4_GPT_7.py
@@ -81,7 +81,27 @@ gen.send()
 
 '''
 
+def auto_start(gen_func):
 
+    def wrapper(*args, **kwargs):
+
+        gen = gen_func(*args, **kwargs)
+        next(gen)
+        return gen
+
+    return wrapper
+
+
+@auto_start
+def accumulator():
+
+    total = 0
+
+    while True:
+        x = yield total
+
+        if x is not None:
+            total += x
 
 
 
@@ -89,6 +109,9 @@ gen.send()
 
 if __name__ == '__main__':
 
+    gen2 = accumulator()
+
+    print(gen2.send(5))
+    print(gen2.send(3))
 
 
-    pass
--- SandBox.py
+++ SandBox.py
@@ -1,24 +1,54 @@
+
+'''
+def change(func):
+
+    def wrapper():
+        print("Before")
+        func()
+        print("After")
+
+    return wrapper
+
+
+def hello():
+    print("Hello")
+
+
+hello = change(hello)
+
+hello()
+hello()
+
+'''
+
+'''change(hello())
+
+hello()'''
+
+def auto_start(gen_func):
+
+    def wrapper(*args, **kwargs):
+
+        gen = gen_func(*args, **kwargs)
+        next(gen)
+        return gen
+
+    return wrapper
+
+
+@auto_start
 def accumulator():
-    accum = 0
+
+    total = 0
+
     while True:
-        x = yield accum
+        x = yield total
 
         if x is not None:
-            accum += x
-
-        print(f'*****************')
-        print(f'Received    -> {x}')
-        print(f'Accumulator -> {accum}')
-        print(f'*****************')
+            total += x
 
 
+gen2 = accumulator()
 
-gen = accumulator()
-
-print(next(gen))
-print(gen.send(4))
-print(gen.send(5))
-print(gen.send(7))
-
-print(next(gen))
-print(next(gen))
+print(gen2.send(5))
+print(gen2.send(3))
